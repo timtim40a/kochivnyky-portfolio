b9e81bb71018186435ac6b865db2c27193c88a06
Tymur Soroka <timtim40a@gmail.com>
2026-04-14 13:49:19 +0100

    Build initial navbar

--- src/_components/Footer/Footer.tsx
+++ src/_components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 function Footer() {
     return (
         <>
-            <footer className={styles.footer}>
+            <footer className={styles.container}>
                 <p>&copy; 2023-2026 Kochivnyky. All rights reserved.</p>
             </footer>
         </>
--- src/_components/Navbar/Navbar.tsx
+++ src/_components/Navbar/Navbar.tsx
@@ -3,7 +3,18 @@ import styles from "./Navbar.module.css";
 function Navbar() {
     return (
         <>
-            <nav className={styles.navbar}></nav>
+            <nav className={styles.container}>
+                <img
+                    src="/kochivnyky_logo_ENG.png"
+                    alt="Kochivnyky Logo"
+                    className={styles.logo}
+                />
+                <div className={styles.contents}>
+                    <div className={styles.chapter}>Chapter 1</div>
+                    <div className={styles.chapter}>Chapter 2</div>
+                    <div className={styles.chapter}>Chapter 3</div>
+                </div>
+            </nav>
         </>
     );
 }
