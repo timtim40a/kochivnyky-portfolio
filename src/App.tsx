1d3628d8379fe2e30fc283c64ef3d2c3babc6fad
VladZernov <vlad.zernov@yahoo.com>
2026-04-15 22:36:46 +0200

    Added videos

--- src/App.tsx
+++ src/App.tsx
@@ -126,33 +126,15 @@ function App() {
                             dignissimos deserunt. Lorem ipsum dolor sit amet
                         </p>
                         <h2>Video materials</h2>
-                        <div>
-                        <iframe width="640" height="375" src="https://www.youtube.com/embed/eViCPa8kpTw?si=NpwS773xr02sFBfJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
-                        <iframe width="640" height="375" src="https://www.youtube.com/embed/ONUiUO8noJE?si=1sXEmOYQ-IGwMJlA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
-                        <iframe width="640" height="375" src="https://www.youtube.com/embed/FbpdRTpYjm4?si=FaV156eg1zojyqLo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
-                        <iframe width="315" height="560" src="https://www.youtube.com/embed/6nSGKyHS4Ns" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen=""></iframe>
-                        <iframe width="315" height="560" src="https://www.youtube.com/embed/1eQenpx-464" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen=""></iframe>
-                        <iframe width="315" height="560" src="https://www.youtube.com/embed/5yWGWvTFLxs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen=""></iframe>
-                        
-                        </div>
-                        <h2>Contact Us</h2>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Aliquam iste eligendi similique architecto
-                            ducimus distinctio ab magni nemo. Necessitatibus
-                            neque quas labore consequuntur nemo deleniti
-                            repellendus quasi alias illum tempore? Lorem ipsum
-                            dolor sit amet consectetur adipisicing elit. Aliquam
-                            iste eligendi similique architecto ducimus
-                            distinctio ab magni nemo. Necessitatibus neque quas
-                            labore consequuntur nemo deleniti repellendus quasi
-                            alias illum tempore? Lorem ipsum dolor, sit amet
-                            consectetur adipisicing elit. Molestias sunt rerum
-                            illo necessitatibus, mollitia inventore nemo ullam
-                            pariatur distinctio culpa, fuga veniam? Temporibus
-                            assumenda mollitia voluptatem architecto ducimus,
-                            dignissimos deserunt. Lorem ipsum dolor sit amet
+                            <iframe width="640" height="375" src="https://www.youtube.com/embed/eViCPa8kpTw?si=NpwS773xr02sFBfJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
+                            <iframe width="640" height="375" src="https://www.youtube.com/embed/ONUiUO8noJE?si=1sXEmOYQ-IGwMJlA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
+                            <iframe width="640" height="375" src="https://www.youtube.com/embed/FbpdRTpYjm4?si=FaV156eg1zojyqLo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
+                            <iframe width="315" height="560" src="https://www.youtube.com/embed/6nSGKyHS4Ns" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen=""></iframe>
+                            <iframe width="315" height="560" src="https://www.youtube.com/embed/1eQenpx-464" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen=""></iframe>
+                            <iframe width="315" height="560" src="https://www.youtube.com/embed/5yWGWvTFLxs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen=""></iframe>
                         </p>
+                        <h2>Contact Us</h2>
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing
                             elit. Aliquam iste eligendi similique architecto
