65c1b708fcbf74b03de925d1787b508b66573c3b
Tymur Soroka <timtim40a@gmail.com>
2026-04-16 13:23:28 +0100

    Grid layout for videos

--- src/App.tsx
+++ src/App.tsx
@@ -489,9 +489,9 @@ function App() {
                             </p>
                         </div>
                         <h2>Video materials</h2>
-                        <p>
+                        <div className={styles.videoContainer}>
                             <iframe
-                                width="640"
+                                width="740"
                                 height="375"
                                 src="https://www.youtube.com/embed/eViCPa8kpTw?si=NpwS773xr02sFBfJ"
                                 title="YouTube video player"
@@ -499,9 +499,10 @@ function App() {
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                 referrerPolicy="strict-origin-when-cross-origin"
                                 allowFullScreen={true}
+                                className={styles.horizontalVideo}
                             ></iframe>
                             <iframe
-                                width="640"
+                                width="740"
                                 height="375"
                                 src="https://www.youtube.com/embed/ONUiUO8noJE?si=1sXEmOYQ-IGwMJlA"
                                 title="YouTube video player"
@@ -509,9 +510,10 @@ function App() {
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                 referrerPolicy="strict-origin-when-cross-origin"
                                 allowFullScreen={true}
+                                className={styles.horizontalVideo}
                             ></iframe>
                             <iframe
-                                width="640"
+                                width="740"
                                 height="375"
                                 src="https://www.youtube.com/embed/FbpdRTpYjm4?si=FaV156eg1zojyqLo"
                                 title="YouTube video player"
@@ -519,35 +521,39 @@ function App() {
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                 referrerPolicy="strict-origin-when-cross-origin"
                                 allowFullScreen={true}
+                                className={styles.horizontalVideo}
                             ></iframe>
                             <iframe
-                                width="315"
-                                height="560"
+                                width="228"
+                                height="405"
                                 src="https://www.youtube.com/embed/6nSGKyHS4Ns"
                                 title="YouTube video player"
                                 frameBorder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                 allowFullScreen={true}
+                                className={styles.verticalVideo}
                             ></iframe>
                             <iframe
-                                width="315"
-                                height="560"
+                                width="228"
+                                height="405"
                                 src="https://www.youtube.com/embed/1eQenpx-464"
                                 title="YouTube video player"
                                 frameBorder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                 allowFullScreen={true}
+                                className={styles.verticalVideo}
                             ></iframe>
                             <iframe
-                                width="315"
-                                height="560"
+                                width="228"
+                                height="405"
                                 src="https://www.youtube.com/embed/5yWGWvTFLxs"
                                 title="YouTube video player"
                                 frameBorder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                 allowFullScreen={true}
+                                className={styles.verticalVideo}
                             ></iframe>
-                        </p>
+                        </div>
                         <h2>Contact Us</h2>
                         <p>
                             <a
